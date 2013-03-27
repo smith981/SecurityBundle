2b7f1d6cb95ffe358aaa285964d3cc6c1ad2c5e8
Ed Smith <smith981@gmail.com>
2013-03-27 09:23:24 -0500

    Adding files

---
+++ Controller/DefaultController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Smith981\SecurityBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Core\SecurityContext;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use JMS\SecurityExtraBundle\Annotation\Secure;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/login")
+     * @Template("Smith981SecurityBundle:Default:login.html.twig")
+     */
+    public function loginAction()
+    {
+        $request = $this->getRequest();
+        $session = $request->getSession();
+
+        // get the login error if there is one
+        if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
+            $error = $request->attributes->get(
+                SecurityContext::AUTHENTICATION_ERROR
+            );
+        } else {
+            $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
+            $session->remove(SecurityContext::AUTHENTICATION_ERROR);
+        }
+
+        return array(
+                // last username entered by the user
+                'last_username' => $session->get(SecurityContext::LAST_USERNAME),
+                'error'         => $error,
+        );
+    }
+}
---
+++ Smith981SecurityBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Smith981\SecurityBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class Smith981SecurityBundle extends Bundle
+{
+}
